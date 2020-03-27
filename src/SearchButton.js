1d37454c5b3963fb970ff06daf5b723c805af628
borndead <shaulitto1@gmail.com>
2020-03-27 14:48:51 +0100

    done

--- src/SearchButton.js
+++ src/SearchButton.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 import Button from "@material-ui/core/Button";
 
-class SearchButton extends React.Component{
-    render(){
-        const {onSearch, darkMode}=this.props
-        return(
-            <Button
-            variant={'contained'}
-            onClick={onSearch}
-            color={darkMode?'secondary':'primary'}
-            >
-                Search
-            </Button>
-        )
-    }
+class SearchButton extends React.Component {
+  render() {
+    const { onSearch, darkMode } = this.props;
+    return (
+      <Button
+        variant={"contained"}
+        onClick={onSearch}
+        color={darkMode ? "secondary" : "primary"}
+      >
+        Search
+      </Button>
+    );
+  }
 }
 
-export default SearchButton
+export default SearchButton;
